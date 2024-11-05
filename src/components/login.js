e8b216e1b47fc3598418df0f50fcd81a53c5c527
kavyaganesh <ganeshtkavya@gmail.com>
2024-11-05 17:46:12 +0530

    initial changes added

---
+++ src/components/login.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+
+const Login = () => {
+
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col login-section-one"></div>
+                <div className="col login-section-two"><p>Hiii, let's Start</p></div>
+
+            </div>
+            
+        </div>
+    );
+}
+
+export default Login
